f969f547c5f030c5345258023a65def7485b915b
Shao Bin <ty3627@gmail.com>
2017-03-26 00:05:32 +0800

    exec10.py was finished

--- exec10.py
+++ exec10.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+import sys
 
 prices = []
 quantities = []
+total_sum = 0
+tax = 0
 
 
 def checkNumberic(value):
@@ -15,10 +18,12 @@ def checkNumberic(value):
 
 
 def calcu_total(prices, quantities):
+    global total_sum, tax
     for price, quantity in zip(prices, quantities):
         total_sum = total_sum + (price * quantity)
 
-    return total_sum
+    tax = total_sum * (5.5 / 100)
+    return total_sum, tax
 
 
 print("Enter the price of item 1:", end=' ')
@@ -45,7 +50,10 @@ price3 = checkNumberic(input_price_3)
 prices.append(price3)
 print("Enter the quantity of item 3:", end=' ')
 input_quantity_3 = input()
-quantity2 = checkNumberic(input_quantity_3)
+quantity3 = checkNumberic(input_quantity_3)
 quantities.append(quantity3)
 
-print("Subtotal {total}".format(total=calcu_total(prices, quantities)))
+all_total, only_tax = calcu_total(prices, quantities)
+print("Subtotal ${:04.2f}".format(all_total))
+print("Tax ${:04.2f}".format(only_tax))
+print("Total ${:04.2f}".format(all_total + only_tax))
